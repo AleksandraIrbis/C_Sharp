27339b758b3b7f4857dfd4bd990506678dac974b
unknown <Aleks@azurpc-468.azur.local>
2023-03-06 23:55:41 +0300

    sem6dz wip1

---
+++ Sem6dz/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("задача 1");
+Console.WriteLine("Введите число");
+bool check = false;
+int number;
+while (check != true)
+{
+    string array = Console.ReadLine();
+    if (int.TryParse(array, out number))
+    {
+        for (int i = 0; array[i]> 0; i++)
+        {
+           Console.WriteLine(array[i]);
+        } 
+         break;      
+       
+    }
+    else
+    {
+        Console.WriteLine("Не удалось распознать число, поробуйте ещё раз");
+    }
+}
