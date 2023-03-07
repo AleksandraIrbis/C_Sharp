9977ac8df12dbbcc0978d1e0b3b923b121167b80
unknown <Aleks@azurpc-468.azur.local>
2023-03-07 19:42:46 +0300

    dz6 finished

--- Sem6dz/Program.cs
+++ Sem6dz/Program.cs
@@ -1,22 +1,24 @@
-﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 Console.WriteLine("задача 1");
-Console.WriteLine("Введите число");
-bool check = false;
-int number;
-while (check != true)
+Console.WriteLine("Введите элементы через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
 {
-    string array = Console.ReadLine();
-    if (int.TryParse(array, out number))
+    if (array[i] > 0)
     {
-        for (int i = 0; array[i]> 0; i++)
-        {
-           Console.WriteLine(array[i]);
-        } 
-         break;      
-       
-    }
-    else
-    {
-        Console.WriteLine("Не удалось распознать число, поробуйте ещё раз");
+        count++;
     }
 }
+ 
+Console.WriteLine($"Количество положительных элементов: {count}");
+
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1   y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("задача 2");
+Console.WriteLine("Введите числа k1, k2, b1, b2  ");
+int[] numbers=Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+//(b2 - b1) / (k1 - k2)
+double x=(numbers[3] - numbers[2]) /((double)numbers[0] - numbers[1]);
+double y=numbers[0]*x+numbers[2];
+Console.WriteLine($"Точка пересечения прямых будет по координатам {x} и {y}");
