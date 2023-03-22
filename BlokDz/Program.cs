0ebf86de2898bb4df0c98accc08de78584799fd0
unknown <Aleks@azurpc-468.azur.local>
2023-03-22 20:16:05 +0300

    added the code))

--- BlokDz/Program.cs
+++ BlokDz/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("задача 1");
+Console.WriteLine("Введите элементы через пробел: ");
+string[] input=Console.ReadLine().Split();
+string[] group = Array.FindAll(input, element => element.Length<4);
+foreach (var element in group) Console.WriteLine(element);
+
+// заполняем массив с клавы. далее с группу записываем все элементы, длинна которых меньше 4. выводим все элементы в группе последовательно))))
